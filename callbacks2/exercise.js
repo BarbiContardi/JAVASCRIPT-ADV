bfa2c7bbea3c9b102d7a706e613429d049072267
Barbara Contardi <barbi.contardi@gmail.com>
2023-08-29 12:24:26 +0200

    Ejercicio 20

--- callbacks2/exercise.js
+++ callbacks2/exercise.js
@@ -1,5 +1,5 @@
 function repeatHello(callback){
-    let id = setInterval(()=> console.log("Hello"), 1000);
-    setTimeout(()=> clearInterval(id), 5000);
+    setInterval(()=> console.log("Hello"), 1000);
    }
-repeatHello()  
+   
+   repeatHello()  
