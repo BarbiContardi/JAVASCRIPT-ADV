d640b61f10921ead1690f0e9d5387a68b6a4a0ec
Barbara Contardi <barbi.contardi@gmail.com>
2023-08-30 10:19:38 +0200

    Ejercicio 23

---
+++ promise-chaining/exercise.js
@@ -0,0 +1,27 @@
+const isLogged = true;
+
+function isLoggedUser(isLogger) {
+  return new Promise((resolve, reject) => {
+    let rand = Math.random();
+    if (isLogged) {
+      resolve(rand);
+    } else {
+      reject(new Error("isLogger is false"));
+    }
+  });
+}
+
+function getUserName(randNumber) {
+  return new Promise((resolve, reject) => {
+    if (randNumber > 0.5) {
+      resolve({ name: "John", age: 24 });
+    } else {
+      reject(new Error("User don't exist"));
+    }
+  });
+}
+
+isLoggedUser()
+    .then(getUserName)
+    .then((val)=> console.log(val))
+    .catch((err)=> console.error(err))
